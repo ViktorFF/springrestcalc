afcc77d9090963b4ef2e763d1555d3f8d604e9cb
ViktorFF <victor.bahanovich@gmail.com>
2020-02-12 14:30:54 +0300

    fix #2 History was added

--- src/main/java/by/springbootrest/calc/controller/HistoryController.java
+++ src/main/java/by/springbootrest/calc/controller/HistoryController.java
@@ -1,9 +1,24 @@
 package by.springbootrest.calc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping
+@RequestMapping(path = "/history")
 public class HistoryController {
+    private Map<Integer, String> history;
+
+    @Autowired
+    public HistoryController(Map<Integer, String> history) {
+        this.history = history;
+    }
+
+    @GetMapping
+    public Map<Integer, String> getHistory() {
+        return history;
+    }
 }
